e7fef37a4f3c8fbeeae0a7bb1cbeb8bf3a9df895
Luke Davis <luke.n.davis.7@gmail.com>
2026-04-11 23:48:01 -0500

    Try to add swaywm pt1

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,7 @@
   # Enable Broadcom STA driver for Mac
   hardware.enableAllFirmware = true;
   
+security.polkit.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
--- home.nix
+++ home.nix
@@ -7,6 +7,19 @@ in
         (import "${home-manager}/nixos")
     ];
 
+     wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "kitty"; 
+      startup = [
+        # Launch Firefox on start
+        {command = "firefox";}
+      ];
+    };
+  };
+
     home-manager.users.upwrd = {
         # This should be the same value as `system.stateVersion` in
         # your `configuration.nix` file.
