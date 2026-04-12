66ff4d4e35eda8d203eac175dfa5c2aa9a721a58
Luke Davis <luke.n.davis.7@gmail.com>
2026-04-11 19:18:21 -0500

    Flake.nix added

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  home-manager = {url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs";};
+};
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
--- recrank.sh
+++ recrank.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-sudo nixos-rebuild switch -I nixos-config=configuration.nix
-
+sudo nixos-rebuild switch --flake ./flake.nix
