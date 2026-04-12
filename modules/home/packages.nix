4a1580b6f471838905c5ee97f98f268ca6d23573
Luke Davis <luke.n.davis.7@gmail.com>
2026-04-12 14:12:35 -0500

    Updated to be more consistent

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -8,9 +8,6 @@
 
     # Unix tools
     ripgrep # Better `grep`
-    fd
-    sd
-    tree
     gnumake
 
     # Nix dev
@@ -18,7 +15,6 @@
     nil # Nix language server
     nix-info
     nixpkgs-fmt
-
 
     # On ubuntu, we need this less for `man home-configuration.nix`'s pager to
     # work.
@@ -32,13 +28,7 @@
     bat.enable = true;
     # Type `<ctrl> + r` to fuzzy search your shell history
     fzf.enable = true;
-    jq.enable = true;
     # Install btop https://github.com/aristocratos/btop
     btop.enable = true;
-    # Tmate terminal sharing.
-    tmate = {
-      enable = true;
-      #host = ""; #In case you wish to use a server other than tmate.io
-    };
   };
 }
