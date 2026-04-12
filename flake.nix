712b9a3b98e077b2b8ea57fbc6b190db0cf32a36
Luke Davis <luke.n.davis.7@gmail.com>
2026-04-12 00:20:20 -0500

    Trying to switch to declarative flake setup,

--- flake.nix
+++ flake.nix
@@ -1,11 +1,29 @@
 {
-  inputs = {nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  home-manager = {url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs";};
-};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-mangager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        # make home-manager as a module of nixos
+        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          # TODO replace ryan with your own username
+          home-manager.users.upwrd = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        }
+      ];
     };
   };
 }
--- home.nix
+++ home.nix
@@ -1,7 +1,4 @@
 { config, pkgs, ... }:
-let
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz;
-in
 {
     imports = [
         (import "${home-manager}/nixos")
