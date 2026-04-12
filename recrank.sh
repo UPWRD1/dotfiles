6bb965d2bd863e204e4f2cf352e78b96222f541a
Luke Davis <luke.n.davis.7@gmail.com>
2026-04-12 15:14:33 -0500

    Update recrank.sh

--- recrank.sh
+++ recrank.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
+commit_all() {
+  git commit -a
+  git push  
+}
+
 if [ -n "$(git status --porcelain)" ]; then
   echo "Uncommitted changes detected"
   while true; do
     # -p displays the prompt string, -r prevents backslash escaping
-    read -p "Do you want to proceed? (y/n) " yn -n 1 -r
+    read -p "Do you want to commit now? (y/n/FORCE) " yn -n 1 -r
     case $yn in
-        [Yy]* ) echo "Proceeding..."; break;;
-        [Nn]* ) echo "Exiting..."; exit;;
-        * ) echo "Please answer yes or no.";;
+        [Yy]* )
+          echo "Committing changes..."
+          commit_all
+          break
+        ;;
+
+        [Nn]* )
+          echo "Exiting..."
+          exit
+        ;;
+
+        FORCE )
+          break
+        ;;
+
+        * )
+          echo "Please answer yes or no."
+        ;;
     esac
   done
-  git commit -a
-  git push
-else
+ else
   echo "No change since last commit"
 fi
-
+echo "Recranking..."
 nix run .#activate
