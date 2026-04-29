83ebe932d1f05c79b2002a3cb898148f2560062b
Luke Davis <luke.n.davis.7@gmail.com>
2026-04-28 22:37:42 -0500

    Update to niri?

--- flake.nix
+++ flake.nix
@@ -28,6 +28,11 @@
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
