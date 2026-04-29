abc9b4f7b58d99db3220db4604cd2ee451f183a6
Luke Davis <luke.n.davis.7@gmail.com>
2026-04-28 22:00:02 -0500

    change to using nixConfig

--- flake.nix
+++ flake.nix
@@ -30,6 +30,13 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
+
   # Wired using https://nixos-unified.org/guide/autowiring
   outputs =
     inputs:
